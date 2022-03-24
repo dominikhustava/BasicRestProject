192412522f6d31ec12f1abb64864130b96c93b92
dominikhustava <dominikhustava1@gmail.com>
2022-03-24 16:35:02 +0100

    Constructor injection of Utils

--- rest-app-ws/src/main/java/com/simplerestapi/restappws/userservice/UserService.java
+++ rest-app-ws/src/main/java/com/simplerestapi/restappws/userservice/UserService.java
@@ -2,6 +2,7 @@ package com.simplerestapi.restappws.userservice;
 
 import com.simplerestapi.restappws.ui.model.request.UserDetailsRequestModel;
 import com.simplerestapi.restappws.ui.model.response.UserRest;
+import org.springframework.stereotype.Service;
 
 @Service
 public interface UserService {
--- rest-app-ws/src/main/java/com/simplerestapi/restappws/userservice/impl/UserServiceImpl.java
+++ rest-app-ws/src/main/java/com/simplerestapi/restappws/userservice/impl/UserServiceImpl.java
@@ -1,16 +1,27 @@
 package com.simplerestapi.restappws.userservice.impl;
 
+import com.simplerestapi.restappws.shared.Utils;
 import com.simplerestapi.restappws.ui.model.request.UserDetailsRequestModel;
 import com.simplerestapi.restappws.ui.model.response.UserRest;
 import com.simplerestapi.restappws.userservice.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     Map<String, UserRest> users;
+
+    Utils utils;
+
+    @Autowired
+    public UserServiceImpl(Utils utils){
+        this.utils = utils;
+    }
 
     @Override
     public UserRest createUser(UserDetailsRequestModel userDetails) {
@@ -19,7 +30,7 @@ public class UserServiceImpl implements UserService {
         returnValue.setFirstName(userDetails.getFirstName());
         returnValue.setLastName(userDetails.getLastName());
 
-        String userId = UUID.randomUUID().toString();
+        String userId = utils.generateUserId();
         returnValue.setUserID(userId);
 
         if(users==null) users = new HashMap<>();
